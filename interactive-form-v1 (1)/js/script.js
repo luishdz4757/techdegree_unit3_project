ce4f55f21b6c1959a0815b96e0bbdc4d404df380
luishdz4757 <53283167+luishdz4757@users.noreply.github.com>
2019-09-23 21:58:25 -0700

    Update script.js

--- interactive-form-v1 (1)/js/script.js
+++ interactive-form-v1 (1)/js/script.js
@@ -4,30 +4,69 @@ document.getElementById('name').focus();
 //Hides the select theme option
 $('#other-title').hide();
 
+$('#title').change (function () {
+    if ($('#title').val() === 'other'){
+        $('#other-title').show();
+    } else {
+        $('#other-title').hide();
+    }
+})
+
 
 //Updates color field to read "Please Select a T-shirt theme"
-$('#colors-js-puns label').empty();
-$('#colors-js-puns label').append("Please select a T-shirt theme");
+//$('#colors-js-puns label').empty();
+//$('#colors-js-puns label').append("Please select a T-shirt theme");
 
 //Hides the color options from the drop down menu
-$('#color option').hide();
+$('#color').hide();
+$('#colors-js-puns label').hide();
 
 //If theme jspuns shirt is seleected then only jspuns color show
-if ($('#design').val('js puns')('clicked')) {
-    $('#color').val('tomato').hide();
-    $('#color').val('steelblue').hide();
-    $('#color').val('dimgrey').hide();
-    $('#color').val('cornflowerblue').show();
-    $('#color').val('darkslategrey').show();
-    $('#color').val('gold').show();
+ $('#design').change (function ()  { 
+    if ($('#design').val() === 'js puns') {
+        $('#color').show();
+        $('#color-js-puns label').show();
+        $("#color option[value='tomato']").hide();
+        $("#color option[value='steelblue']").hide();
+        $("#color option[value='dimgrey']").hide();
+        $("#color option[value='cornflowerblue']").show();
+        $("#color option[value='darkslategrey']").show();
+        $("#color option[value='gold']").show();
+        } else if ($('#design').val() === 'heart js') {
+            $('#color').show();
+            $('#color-js-puns label').show();
+            $("#color option[value='tomato']").show().attr('selected', 'selected');
+            $("#color option[value='steelblue']").show();
+            $("#color option[value='dimgrey']").show();
+            $("#color option[value='cornflowerblue']").hide();
+            $("#color option[value='darkslategrey']").hide();
+            $("#color option[value='gold']").hide();
+    }
+});
 
-    //While if other shirt theme is selected only that corresponing colors show up as well.
-}; else {
-    $('#color').val('tomato').show();
-    $('#color').val('steelblue').show();
-    $('#color').val('dimgrey').show();
-    $('#color').val('cornflowerblue').hide();
-    $('#color').val('darkslategrey').hide();
-    $('#color').val('gold').hide();
-}
+// Activity Section
+
+//Create element to display total cost and append it to "".activity"
+const activityElement = '<span></span>'
+$('.activities').append(activityElement);
+let totalActivityCost = 0;
+
+//Change listener
+$('.activities').change (function (e) {
+   const $elementTarget = e.target;
+   const $dataCostActivity = $($elementTarget).attr('data-cost');
+//Adding cost
+    if ($elementTarget.attr('checked') === true {
+       //cost of individual activity
+        activityCost =  $dataCostActivity ;
+       totalActivityCost += activityCost;
+//Taking off cost, subtracting   
+    } else {
+
+        totalActivityCost = totalActivityCost - activityCost;
+    }
+
+});
+
+
 
